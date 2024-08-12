d026afa39f9978995fa08856c36880ae419ba072
Nomad <nomad@storm.com>
2024-08-12 12:17:12 +0530

    changed connection

--- ui.py
+++ ui.py
@@ -9,7 +9,7 @@ class TicTacToeGUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Tic Tac Toe")
-        self.api_url = "http://35.175.188.29:30007"  # Update with the actual API URL
+        self.api_url = "http://54.234.227.196:30007"  # Update with the actual API URL
         self.player_name = self.get_player_name()
         self.game = TicTacToe()
         self.x_player = HumanPlayer('X', self.player_name)
